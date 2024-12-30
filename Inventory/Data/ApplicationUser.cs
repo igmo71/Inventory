557f7883c80a42f257a39b8bc9f0f9eef6b5770a
Igor Mogilnitskiy <igmo71@outlook.com>
2024-12-30 15:24:24 +0300

    0002 Add Name and Location to the ApplicationUser.

--- Inventory/Data/ApplicationUser.cs
+++ Inventory/Data/ApplicationUser.cs
@@ -6,10 +6,9 @@ namespace Inventory.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-        //public string? Name { get; set; }
+        public string? Name { get; set; }
 
-        //public Location? Location { get; set; }
-
-        //public string? LocationId { get; set; }
+        public Location? Location { get; set; }
+        public string? LocationId { get; set; }
     }
 }
---
+++ Inventory/Data/Migrations/20241230115549_UserNameAndLocation.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventory.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserNameAndLocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LocationId",
+                table: "AspNetUsers",
+                type: "nvarchar(36)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_LocationId",
+                table: "AspNetUsers",
+                column: "LocationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Locations_LocationId",
+                table: "AspNetUsers",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Locations_LocationId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_LocationId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LocationId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+        }
+    }
+}

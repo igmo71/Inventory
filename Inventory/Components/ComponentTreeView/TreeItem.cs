6c8e25f06f8cbb4ff3cbbb91eda6c1c755218640
Igor Mogilnitskiy <igmo71@outlook.com>
2025-01-21 14:40:50 +0300

    0006 TreeTable - Attempts. ComponentTreeView.

---
+++ Inventory/Components/ComponentTreeView/TreeItem.cs
@@ -0,0 +1,19 @@
+﻿namespace Inventory.Components.ComponentTreeView
+{
+    public class TreeItem
+    {
+        public Guid Id { get; set; }
+
+        public string? Value { get; set; }
+
+        public TreeItem? Parent { get; set; }
+
+        public List<TreeItem>? Children { get; set; }
+
+        public bool HasChildren { get; set; }
+
+        public bool IsFolder { get; set; }
+
+        public bool IsExpanded { get; set; }
+    }
+}
